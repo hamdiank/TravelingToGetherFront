3cb94555ce14032d76f6bb1ddb3c6b970057ad9f
hamdiank <hamdi.ankoud@gmail.com>
2017-04-06 15:37:53 +0100

    az

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
+var appHigh_module_1 = require('./appHigh.module');
 var core_1 = require('@angular/core');
 core_1.enableProdMode();
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
+platform.bootstrapModule(appHigh_module_1.AppHighModule);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { AppHighModule } from './appHigh.module';
 import {enableProdMode} from '@angular/core';
 enableProdMode();
 const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+platform.bootstrapModule(AppHighModule);
